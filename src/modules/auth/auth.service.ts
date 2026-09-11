cf7590639177aec180c523dab0080f34fe19a4b2
Tanjim Siddiki Siyam <siam88536@gmail.com>
2026-09-11 22:42:10 +0600

    feat: implement login functionality with JWT token generation

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import bcrypt from "bcrypt";
 import prisma from "../../lib/prisma.js";
-import type { RegisterInput } from "./auth.schema.js";
+import type { LoginInput, RegisterInput } from "./auth.schema.js";
+import jwt from "jsonwebtoken";
+import { env } from "../../config/env.js";
 
 export const registerUser = async (data: RegisterInput) => {
   const existingUser = await prisma.user.findUnique({
@@ -29,5 +31,47 @@ export const registerUser = async (data: RegisterInput) => {
     email: user.email,
     role: user.role,
     createdAt: user.createdAt,
+  };
+};
+
+export const loginUser = async (data: LoginInput) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      email: data.email,
+    },
+  });
+
+  if (!user) {
+    throw new Error("Invalid email or password");
+  }
+
+  const passwordMatch = await bcrypt.compare(
+    data.password,
+    user.password
+  );
+
+  if (!passwordMatch) {
+    throw new Error("Invalid email or password");
+  }
+
+  const accessToken = jwt.sign(
+    {
+      userId: user.id,
+      role: user.role,
+    },
+    env.JWT_ACCESS_SECRET,
+    {
+      expiresIn: "15m",
+    }
+  );
+
+  return {
+    accessToken,
+    user: {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    },
   };
 };
