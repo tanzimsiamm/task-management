12cda0b93aa76fb7e3c5796e22cf4dd5d95bcea9
Tanjim Siddiki Siyam <siam88536@gmail.com>
2026-09-25 22:21:19 +0600

    feat: add getAll endpoint to retrieve all projects

--- src/modules/projects/project.route.ts
+++ src/modules/projects/project.route.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { authenticate } from "../../middleware/auth.js";
-import { create } from "./project.controller.js";
+import { create, getAll } from "./project.controller.js";
 
 const router = Router();
 
 router.post("/", authenticate, create);
+router.get("/", authenticate, getAll);
 
 export default router;
 
